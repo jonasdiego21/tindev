e4477c647aae341a8051706db30b57ab646e4d99
Jonas Diego Rodrigues Martins <jonasdiegorodriguesmartins@yahoo.com.br>
2019-10-23 13:16:28 -0300

    Update api.js

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'http://138.94.25.30:3333',
-    baseURL: 'http://192.168.88.194:3333',
+    baseURL: 'http://ip:port',
 });
 
 export default api;
